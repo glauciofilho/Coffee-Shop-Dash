17c8a96ba3506d74a02016fd5c45b2f2aca637b0
Gláucio <glauciofilho@live.com>
2025-03-08 17:49:32 -0300

    First bar test

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 import pandas as pd
+from dash import Dash, html, dcc
+import plotly.express as px
+
+app = Dash()
 
 df = pd.read_csv(r".\coffee_shop_revenue.csv")
 
-print (df)
+fig = px.bar(df, x="Number_of_Employees", y="Number_of_Customers_Per_Day", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
